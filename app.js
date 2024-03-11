81781b60e0e56ad6ff936702573b78629d53a80b
JakubKotulski <jakub.kotulski@interia.com>
2024-03-11 09:05:17 +0100

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, "https://www.linkpicture.com/"],
+    credentials: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("Server has started");
+});
